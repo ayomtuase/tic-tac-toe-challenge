50473df5ae1a21c30adf68118e5b6ad946a4d2ce
Ayomide Oguntuase <oguntuase3@gmail.com>
2022-10-25 09:07:05 +0100

    Updated master with initial challenge template

--- src/App.tsx
+++ src/App.tsx
@@ -49,148 +49,36 @@ const buttonStyle = {
   fontSize: "16px"
 };
 
-function Square({
-  squareState,
-  handleSquareClick,
-  rowIndex,
-  squareIndex
-}: {
-  squareState: string;
-  handleSquareClick: (rowIndex: number, squareIndex: number) => void;
-  rowIndex: number;
-  squareIndex: number;
-}) {
-  return (
-    <div
-      className="square"
-      onClick={() => handleSquareClick(rowIndex, squareIndex)}
-      style={squareStyle}
-    >
-      {squareState}
-    </div>
-  );
+function Square() {
+  return <div className="square" style={squareStyle}></div>;
 }
 
 function Board() {
-  const [nextPlayer, setNextPlayer] = useState("X");
-
-  const [winner, setWinner] = useState("");
-
-  const [boardState, setBoardState] = useState([
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ]);
-
-  React.useEffect(() => {
-    //horizontal win check
-    for (let i = 0; i < boardState.length; i++) {
-      if (boardState[i].every((square) => square === "X")) {
-        setWinner((s) => "X");
-      }
-      if (boardState[i].every((square) => square === "O")) {
-        setWinner("O");
-      }
-    }
-
-    //vertical win check
-    for (let i = 0; i < boardState.length; i++) {
-      if (
-        boardState[0][i] === "X" &&
-        boardState[1][i] === "X" &&
-        boardState[2][i] === "X"
-      ) {
-        setWinner("X");
-      }
-      if (
-        boardState[0][i] === "O" &&
-        boardState[1][i] === "O" &&
-        boardState[2][i] === "O"
-      ) {
-        setWinner("O");
-      }
-    }
-
-    //diagonal win check
-    if (
-      boardState[0][0] === "X" &&
-      boardState[1][1] === "X" &&
-      boardState[2][2] === "X"
-    ) {
-      setWinner("X");
-    }
-    if (
-      boardState[0][0] === "O" &&
-      boardState[1][1] === "O" &&
-      boardState[2][2] === "O"
-    ) {
-      setWinner("O");
-    }
-    if (
-      boardState[0][2] === "X" &&
-      boardState[1][1] === "X" &&
-      boardState[2][0] === "X"
-    ) {
-      setWinner("X");
-    }
-    if (
-      boardState[0][2] === "O" &&
-      boardState[1][1] === "O" &&
-      boardState[2][0] === "O"
-    ) {
-      setWinner("O");
-    }
-  }, [...boardState[0], ...boardState[1], ...boardState[2]]);
-
-  const handleSquareClick = (rowIndex: number, squareIndex: number) => {
-    if (boardState[rowIndex][squareIndex] !== "") return;
-    setBoardState((bs) => {
-      bs[rowIndex][squareIndex] = nextPlayer;
-      return bs;
-    });
-
-    if (nextPlayer === "X") {
-      setNextPlayer("O");
-    } else {
-      setNextPlayer("X");
-    }
-  };
-
-  const handleResetClick = () => {
-    setBoardState([
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
-    ]);
-    setNextPlayer("X");
-    setWinner("");
-  };
-
   return (
     <div style={containerStyle} className="gameBoard">
       <div id="statusArea" className="status" style={instructionsStyle}>
-        Next player: {nextPlayer}
+        Next player:
       </div>
       <div id="winnerArea" className="winner" style={instructionsStyle}>
-        Winner: <span>{winner}</span>
+        Winner: <span>None</span>
       </div>
-      <button style={buttonStyle} onClick={handleResetClick}>
-        Reset
-      </button>
+      <button style={buttonStyle}>Reset</button>
       <div style={boardStyle}>
-        {boardState.map((_, rowIndex) => (
-          <div key={rowIndex} className="board-row" style={rowStyle}>
-            {boardState[rowIndex].map((_, squareIndex) => (
-              <Square
-                key={squareIndex}
-                rowIndex={rowIndex}
-                squareIndex={squareIndex}
-                squareState={boardState[rowIndex][squareIndex]}
-                handleSquareClick={handleSquareClick}
-              />
-            ))}
-          </div>
-        ))}
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
       </div>
     </div>
   );
